0f7ec305113c323d3fc3c3f3393d07af8c1578e4
roniantonius <roniantoniuss@upi.edu>
2024-08-16 21:55:19 +0700

    module 5: Apache runtime, architecture, environment

---
+++ 12. Introduction to Big Data with Spark and Hadoop/5_1_Submit Apache Spark Application.py
@@ -0,0 +1,28 @@
+import findspark
+findspark.init()
+
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StrudctType, IntegerType, StringType
+
+sc = SparkContext.getOrCreate(SparkConf().setMaster('spark://localhost:7077'))
+sc.setLogLevel("INFO")
+
+spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.createDataFrame(
+    [
+        (1, "food")
+        (2, "bar"),
+    ],
+    StructType(
+        [
+            StructField("id", IntegerType(), False),
+            StructField("txt", StringType(), False),
+        ]
+    ),
+)
+
+print(df.dtypes)
+df.show
